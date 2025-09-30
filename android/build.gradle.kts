bb8d5c74dae80760f2a030aa08c1a0d00439564a
maolilup <66016766+maolilup@users.noreply.github.com>
2025-10-01 00:25:21 +0800

    翻译2

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+// 确保所有子项目在应用模块之后进行评估
 subprojects {
     project.evaluationDependsOn(":app")
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,15 +1,20 @@
+// Gradle插件管理配置
 pluginManagement {
+    // 获取Flutter SDK路径
     val flutterSdkPath =
         run {
             val properties = java.util.Properties()
             file("local.properties").inputStream().use { properties.load(it) }
             val flutterSdkPath = properties.getProperty("flutter.sdk")
+            // 检查local.properties文件中是否已设置flutter.sdk
             require(flutterSdkPath != null) { "flutter.sdk未在local.properties中设置" }
             flutterSdkPath
         }
 
+    // 包含Flutter工具的Gradle构建脚本
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
+    // 配置插件仓库
     repositories {
         google()
         mavenCentral()
@@ -17,10 +22,12 @@ pluginManagement {
     }
 }
 
+// 应用项目所需的Gradle插件
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
+// 包含应用模块
 include(":app")
